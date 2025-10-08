f9e7e2256c291bd610fa09e6d83316106a1f0e55
Ramiro <rberrelleza@gmail.com>
2025-10-08 16:51:38 -0700

    rename env vars to be compatible with https://www.okteto.com/docs/get-started/deploy-your-app/

--- init.js
+++ init.js
@@ -1,8 +1,8 @@
 // init.js will be executed when the container and the volume are first created
 db.createUser(
   {
-    user: process.env.MONGO_INITDB_NONROOT_USERNAME,
-    pwd: process.env.MONGO_INITDB_NONROOT_PASSWORD,
+    user: process.env.MONGODB_USERNAME,
+    pwd: process.env.MONGODB_PASSWORD,
     roles: [ { role: "readWrite", db: process.env.MONGO_INITDB_DATABASE } ]
   }
 )
